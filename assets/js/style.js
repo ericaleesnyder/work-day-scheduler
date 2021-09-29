8dfd95af824ca723027fb1c9e426754550309564
Erica Snyder <ericaleesnyder@gmail.com>
2021-09-29 10:42:31 -0700

    initial commit

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+// HEADER
+// show current date (moment)
+
+// CONTAINER
+// color coding for past present and future
+// classes already exist with styles
+// need the program to be able to read what time it currently is (moment) and apply classes accordingly 
+
+// SAVE BUTTON
+// saves entry to local storage
+// needs render function to display on the schedule after refresh
